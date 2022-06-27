5acb4189f516a86f65ad626af8e23b484cbe11b2
Ryan Cabanas <ryangithub777@gmail.com>
2022-06-27 15:55:52 -0400

    Testing out custom error classes that extend base `Error` class.  Particular emphasis was on understanding how to set and get to the custom error name and how to still get to the parent error class name.

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-const isValid = false;
-console.log(isValid && 'end');
-console.log(!isValid && 'end');
-console.log('this' && 'that');
+class MyError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name; // Must explicitly set the name of the custom error in this class if you want to use later.
+    this.supername = super.name; // You can still get to the parent class name, you want.
+  }
+}
+
+const testError = new MyError('test error message');
+console.log(testError.message);
+console.log(testError.name);
+console.log(testError.supername);
